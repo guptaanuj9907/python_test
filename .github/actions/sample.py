0b3d7a39038886d8561352931aa812b6a32ab057
Anuj Gupta <anuj.gupta@razorpay.com>
2023-02-07 22:27:43 +0530

    p1

--- .github/actions/sample.py
+++ .github/actions/sample.py
@@ -69,14 +69,14 @@ def main():
     print("s3_iam_dir_present = ",s3_iam_dir_present)
     block_dir=get_block_directory_list()
     print("block directories list = ",block_dir)
-    # print("compare_file_changed_and_block_directory = ",compare_file_changed_and_block_directory(file_changed=file_changed,block_directory=block_dir))
-    # if s3_iam_dir_present:
-    #     if compare_file_changed_and_block_directory(
-    #         file_changed=file_changed,block_directory=block_dir):
-    #         close_pr()
-    #     else:
-    #         print("No Drift !!!!!!!!!!!!!")
-    #         print("Trigger the cron job again")
+    print("compare_file_changed_and_block_directory = ",compare_file_changed_and_block_directory(file_changed=file_changed,block_directory=block_dir))
+    if s3_iam_dir_present:
+        if compare_file_changed_and_block_directory(
+            file_changed=file_changed,block_directory=block_dir):
+            close_pr()
+        else:
+            print("No Drift !!!!!!!!!!!!!")
+            print("Trigger the cron job again")
     
 
 
