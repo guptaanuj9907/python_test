3624aa92a017a51be1ee8179f8525b2e3517d0c4
Anuj Gupta <anuj.gupta@razorpay.com>
2023-02-07 18:06:03 +0530

    main

---
+++ .github/actions/sample.py
@@ -0,0 +1,41 @@
+import os
+import json
+import logging
+import datetime
+import requests
+
+
+
+
+
+
+
+
+
+
+
+def get_pr_number():
+    ref = os.getenv('GITHUB_REF')
+    arr = ref.split("/")
+    print("ref "+str(ref))
+    print(arr)
+    return arr[2]
+
+
+def main():
+    token = os.getenv('GIT_TOKEN')
+    head = {'Authorization': 'token ' + token}
+    url = "https://api.github.com/repos/razorpay/vishnu/pulls/" + str(get_pr_number())
+    payload = {"state":"closed","base":"main"}
+    r = requests.patch(url=url, headers=head, data = json.dumps(payload))
+    print("token"+" "+str(token))
+    print("url"+" "+url)
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
