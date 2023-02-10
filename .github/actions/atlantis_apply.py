0d954e525724c93bc5ffab9e406ccc6822d0e4ad
Anuj Gupta <anuj.gupta@razorpay.com>
2023-02-10 14:07:03 +0530

    p1

--- .github/actions/atlantis_apply.py
+++ .github/actions/atlantis_apply.py
@@ -1,30 +1,37 @@
 import os
 import requests
 
-## Get the value of GITHUB_REPOSITORY and extract the owner
-print("ENV = ")
-print(os.environ)
-# owner_and_repo = "razorpay/vishnu"
-owner_and_repo = "guptaanuj9907/python_test"
+def auto_merge():
+  ## Get the value of GITHUB_REPOSITORY and extract the owner
+  print("ENV = ")
+  print(os.environ)
+  # owner_and_repo = "razorpay/vishnu"
+  owner_and_repo = "guptaanuj9907/python_test"
 
-# Get the pull request number
-pull_request_number = os.environ['GITHUB_EVENT_PULL_REQUEST_NUMBER']
+  # Get the pull request number
+  pull_request_number = os.environ['GITHUB_EVENT_PULL_REQUEST_NUMBER']
 
-# Check if atlantis apply ran successfully
-# write logic here to determine if atlantis apply ran successfully
-token = os.getenv('GIT_TOKEN')
-header = {'Authorization': 'token ' + token,"Accept": "application/vnd.github+json"}
-url = "https://api.github.com/repos/"+owner_and_repo+"/pulls/" + str(pull_request_number)+"/merge"
-print(url)
-data = {
-    "commit_title": "Merge pull request",
-    "commit_message": "Merging pull request"
-}
-response = requests.put(url=url, headers=header,json=data)
-print(response)
-if response.status_code == 200:
-    print("Merge successful")
-else:
-    print("Merge failed")
+#write a logic to check whether atlantis apply ran successfully or not
+#if it ran successfully then run atlantis plan and check the drift
+#if their is not drift merge it
+#else raise a jira ticket 
+
+  token = os.getenv('GIT_TOKEN')
+  header = {'Authorization': 'token ' + token,"Accept": "application/vnd.github+json"}
+  url = "https://api.github.com/repos/"+owner_and_repo+"/pulls/" + str(pull_request_number)+"/merge"
+  print(url)
+  data = {
+      "commit_title": "Merge pull request",
+      "commit_message": "Merging pull request"
+  }
+  response = requests.put(url=url, headers=header,json=data)
+  print(response)
+  if response.status_code == 200:
+      print("Merge successful")
+  else:
+      print("Merge failed")
+
+if __name__ == "__main__":
+    auto_merge()
 
     ####
