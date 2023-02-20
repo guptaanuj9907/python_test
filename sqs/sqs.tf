1ab88df2869fc8391af6af690f50dc3fcf9b7063
Anuj Gupta <anuj.gupta@razorpay.com>
2023-02-20 15:02:24 +0530

    p1

--- sqs/sqs.tf
+++ sqs/sqs.tf
@@ -1,113 +1,16 @@
-# resource "aws_route53_record" "scrooge" {}
-SELENIUM_JAR_PATH=/usr/share/java/selenium-server.jar
-GOROOT_1_17_X64=/opt/hostedtoolcache/go/1.17.13/x64
-CONDA=/usr/share/miniconda
-GITHUB_WORKSPACE=/home/runner/work/python_test/python_test
-JAVA_HOME_11_X64=/usr/lib/jvm/temurin-11-jdk-amd64
-GITHUB_PATH=/home/runner/work/_temp/_runner_file_commands/add_path_3fdd1421-d4b9-407b-b95b-8fc9ea68b0bb
-GITHUB_ACTION=__run
-JAVA_HOME=/usr/lib/jvm/temurin-11-jdk-amd64
-GITHUB_RUN_NUMBER=18
-RUNNER_NAME=Hosted Agent
-GRADLE_HOME=/usr/share/gradle-7.6
-GITHUB_REPOSITORY_OWNER_ID=67305929
-XDG_CONFIG_HOME=/home/runner/.config
-DOTNET_SKIP_FIRST_TIME_EXPERIENCE=1
-ANT_HOME=/usr/share/ant
-JAVA_HOME_8_X64=/usr/lib/jvm/temurin-8-jdk-amd64
-GITHUB_TRIGGERING_ACTOR=guptaanuj9907
-pythonLocation=/opt/hostedtoolcache/Python/3.8.16/x64
-GITHUB_REF_TYPE=branch
-HOMEBREW_CLEANUP_PERIODIC_FULL_DAYS=3650
-ANDROID_NDK=/usr/local/lib/android/sdk/ndk/25.2.9519653
-BOOTSTRAP_HASKELL_NONINTERACTIVE=1
-***
-PIPX_BIN_DIR=/opt/pipx_bin
-GITHUB_REPOSITORY_ID=598576064
-DEPLOYMENT_BASEPATH=/opt/runner
-GITHUB_ACTIONS=true
-ANDROID_NDK_LATEST_HOME=/usr/local/lib/android/sdk/ndk/25.2.9519653
-SYSTEMD_EXEC_PID=667
-GITHUB_SHA=ee64382bd37be0014a91ff76dd18b2aa68ef5c94
-GITHUB_WORKFLOW_REF=guptaanuj9907/python_test/.github/workflows/pr_created.yml@refs/pull/122/merge
-POWERSHELL_DISTRIBUTION_CHANNEL=GitHub-Actions-ubuntu22
-DOTNET_MULTILEVEL_LOOKUP=0
-GITHUB_REF=refs/pull/122/merge
-RUNNER_OS=Linux
-GITHUB_REF_PROTECTED=false
-HOME=/home/runner
-GITHUB_API_URL=https://api.github.com
-LANG=C.UTF-8
-RUNNER_TRACKING_ID=github_3624c15a-2cb7-42f9-a5bf-f2eac641ea56
-ACTIONS_ID_TOKEN_REQUEST_TOKEN=***
-RUNNER_ARCH=X64
-RUNNER_TEMP=/home/runner/work/_temp
-GITHUB_STATE=/home/runner/work/_temp/_runner_file_commands/save_state_3fdd1421-d4b9-407b-b95b-8fc9ea68b0bb
-EDGEWEBDRIVER=/usr/local/share/edge_driver
-GITHUB_ENV=/home/runner/work/_temp/_runner_file_commands/set_env_3fdd1421-d4b9-407b-b95b-8fc9ea68b0bb
-GITHUB_EVENT_PATH=/home/runner/work/_temp/_github_workflow/event.json
-INVOCATION_ID=118d3649aee14c648c2a84f2c484b626
-GITHUB_EVENT_NAME=pull_request
-GITHUB_RUN_ID=4184120432
-JAVA_HOME_17_X64=/usr/lib/jvm/temurin-17-jdk-amd64
-ANDROID_NDK_HOME=/usr/local/lib/android/sdk/ndk/25.2.9519653
-GITHUB_STEP_SUMMARY=/home/runner/work/_temp/_runner_file_commands/step_summary_3fdd1421-d4b9-407b-b95b-8fc9ea68b0bb
-HOMEBREW_NO_AUTO_UPDATE=1
-GITHUB_ACTOR=guptaanuj9907
-NVM_DIR=/home/runner/.nvm
-SGX_AESM_ADDR=1
-GITHUB_RUN_ATTEMPT=1
-STATS_RDCL=true
-ANDROID_HOME=/usr/local/lib/android/sdk
-GITHUB_GRAPHQL_URL=https://api.github.com/graphql
-ACCEPT_EULA=Y
-RUNNER_USER=runner
-USER=runner
-GITHUB_SERVER_URL=https://github.com
-PIPX_HOME=/opt/pipx
-GECKOWEBDRIVER=/usr/local/share/gecko_driver
-STATS_NM=true
-CHROMEWEBDRIVER=/usr/local/share/chrome_driver
-SHLVL=1
-ANDROID_SDK_ROOT=/usr/local/lib/android/sdk
-VCPKG_INSTALLATION_ROOT=/usr/local/share/vcpkg
-GITHUB_ACTOR_ID=67305929
-RUNNER_TOOL_CACHE=/opt/hostedtoolcache
-ImageVersion=20230206.1
-DOTNET_NOLOGO=1
-GITHUB_WORKFLOW_SHA=ee64382bd37be0014a91ff76dd18b2aa68ef5c94
-GITHUB_REF_NAME=122/merge
-GRAALVM_11_ROOT=/usr/local/graalvm/graalvm-ce-java11-22.3.1
-GITHUB_JOB=check_prs
-LD_LIBRARY_PATH=/opt/hostedtoolcache/Python/3.8.16/x64/lib
-XDG_RUNTIME_DIR=/run/user/1001
-ACTIONS_ID_TOKEN_REQUEST_URL=https://pipelines.actions.githubusercontent.com/VFAuwwMyjntkqA5nF7lkwWe6vlSdHEgOJIzcBBUL5LNRkyBDuR/00000000-0000-0000-0000-000000000000/_apis/distributedtask/hubs/Actions/plans/1bee6fbf-7ef4-4f48-91c7-5323d0b1ba48/jobs/487dd9b0-cebf-5622-4e00-96320ab04555/idtoken?api-version=2.0
-AZURE_EXTENSION_DIR=/opt/az/azcliextensions
-PERFLOG_LOCATION_SETTING=RUNNER_PERFLOG
-GITHUB_REPOSITORY=guptaanuj9907/python_test
-CHROME_BIN=/usr/bin/google-chrome
-ANDROID_NDK_ROOT=/usr/local/lib/android/sdk/ndk/25.2.9519653
-GOROOT_1_18_X64=/opt/hostedtoolcache/go/1.18.10/x64
-GITHUB_RETENTION_DAYS=90
-JOURNAL_STREAM=8:16810
-RUNNER_WORKSPACE=/home/runner/work/python_test
-LEIN_HOME=/usr/local/lib/lein
-LEIN_JAR=/usr/local/lib/lein/self-installs/leiningen-2.10.0-standalone.jar
-GITHUB_ACTION_REPOSITORY=
-PATH=/opt/hostedtoolcache/Python/3.8.16/x64/bin:/opt/hostedtoolcache/Python/3.8.16/x64:/home/runner/.local/bin:/opt/pipx_bin:/home/runner/.cargo/bin:/home/runner/.config/composer/vendor/bin:/usr/local/.ghcup/bin:/home/runner/.dotnet/tools:/snap/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin
-RUNNER_PERFLOG=/home/runner/perflog
-GITHUB_BASE_REF=main
-GHCUP_INSTALL_BASE_PREFIX=/usr/local
-CI=true
-SWIFT_PATH=/usr/share/swift/usr/bin
-ImageOS=ubuntu22
-GITHUB_REPOSITORY_OWNER=guptaanuj9907
-GITHUB_HEAD_REF=p1
-GIT_TOKEN=***
-GITHUB_ACTION_REF=
-GOROOT_1_19_X64=/opt/hostedtoolcache/go/1.19.5/x64
-GITHUB_WORKFLOW=SDLC
-DEBIAN_FRONTEND=noninteractive
-GITHUB_OUTPUT=/home/runner/work/_temp/_runner_file_commands/set_output_3fdd1421-d4b9-407b-b95b-8fc9ea68b0bb
-AGENT_TOOLSDIRECTORY=/opt/hostedtoolcache
-_=/usr/bin/env
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "s3:Get*",
+                "s3:List*",
+                "s3-object-lambda:Get*",
+                "s3-object-lambda:List*"
+            ],
+            "Resource": "*"
+        }
+    ]
+}
+AmazonS3ReadOnlyAccess
