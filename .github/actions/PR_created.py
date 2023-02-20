bbc73da1190678d1a15e7f8757b575652501f474
Anuj Gupta <anuj.gupta@razorpay.com>
2023-02-20 17:22:17 +0530

    p1

--- .github/actions/PR_created.py
+++ .github/actions/PR_created.py
@@ -90,10 +90,12 @@ def get_block_directory_list():
 
     # Print the contents of the file
     print(file_contents)
-
+    block_dir=[]
     # block_dir = [line.strip().replace('"', '') for line in file_contents.split('\n') if line.strip() and len(line.strip()) > 0]
     for line in file_contents.split('\n'):
-        print("line :",line)
+        print("line :",line,len(line))
+        if len(line)>0:
+            block_dir.append(line)
     print("block_dir")
     # print(block_dir)
     # return block_dir
