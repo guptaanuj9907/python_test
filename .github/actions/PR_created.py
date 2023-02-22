0c51fa1dad629ae393583e9ef4eea2872ddcb353
Anuj Gupta <anuj.gupta@razorpay.com>
2023-02-22 19:40:56 +0530

    p1

--- .github/actions/PR_created.py
+++ .github/actions/PR_created.py
@@ -4,8 +4,48 @@ import logging
 import datetime
 import requests
 
+
 # owner_and_repo = "razorpay/vishnu"
 owner_and_repo = "guptaanuj9907/python_test"
+
+def html_content():
+    import os
+    import webbrowser
+    from flask import  render_template
+    from jinja2 import Template
+    import os
+
+    cwd = os.getcwd()
+    print("Current working directory:", cwd)
+
+    # html_file_path = '/Users/anuj.gupta/Desktop/sdlc/python_test/python_test/s3/index.html'
+    # html_file_path=cwd+"/s3/index.html"
+    html_file_path=cwd+"/.github/html_content"
+    pull_request_number=123
+    # webbrowser.open('file://' + os.path.realpath(html_file_path) + f'?pull_request_number={pull_request_number}')
+
+
+    with open(html_file_path+"/index.html") as f:
+        template = Template(f.read())
+
+    # Define the values to be passed to the template
+    pull_request_number = 56745
+    drift = "driftttttt"
+
+    # Render the template with the values
+    output_html = template.render(pull_request_number=pull_request_number, drift=drift)
+
+    # Print the rendered HTML
+    print(output_html)
+
+    # Save the HTML content to a file
+    with open(html_file_path+"/output.html", 'w') as f:
+        f.write(output_html)
+
+    # Open the file in a web browser
+    webbrowser.open("file://"+html_file_path+"/output.html")
+
+
 
 def get_pr_number():
     """
@@ -130,7 +170,6 @@ def comment_on_pr(pr_no):
             blocked_emails[email].append(directory)
         else:
             blocked_emails[email] = [directory]
-    print(blocked_emails)
     token = os.getenv('GIT_TOKEN')
     head = {'Authorization': 'token ' + token}
     url = "https://api.github.com/repos/"+owner_and_repo+"/issues/" + str(pr_no) + "/comments"
@@ -177,6 +216,7 @@ def main():
             print("PR is not being closed since it created by drift creator : {}".format(user_github_id))
             close_pr()
             comment_on_pr(get_pr_number())
+            html_content()
     else:
         print("File change file path is NOT present in BLOCK DIRECTORY")
         print("No Drift !!!!!!!!!!!!!...Trigger the cron job again when someone run atlantis plan")
--- s3/python.py
+++ s3/python.py
@@ -1,23 +1,36 @@
 import os
 import webbrowser
 from flask import  render_template
-
-# Replace this with the full path to your HTML file
+from jinja2 import Template
 import os
-
 
 cwd = os.getcwd()
 print("Current working directory:", cwd)
 
 # html_file_path = '/Users/anuj.gupta/Desktop/sdlc/python_test/python_test/s3/index.html'
-
-html_file_path=cwd+"/s3/index.html"
-
+# html_file_path=cwd+"/s3/index.html"
+html_file_path=cwd+"/.github/html_content"
 pull_request_number=123
+# webbrowser.open('file://' + os.path.realpath(html_file_path) + f'?pull_request_number={pull_request_number}')
 
-webbrowser.open('file://' + os.path.realpath(html_file_path) + f'?pull_request_number={pull_request_number}')
 
-# webbrowser.open(html_file_path)
+with open(html_file_path+"/index.html") as f:
+    template = Template(f.read())
 
-render_template('index.html')
+# Define the values to be passed to the template
+pull_request_number = 56745
+drift = "driftttttt"
+
+# Render the template with the values
+output_html = template.render(pull_request_number=pull_request_number, drift=drift)
+
+# Print the rendered HTML
+print(output_html)
+
+# Save the HTML content to a file
+with open(html_file_path+"/output.html", 'w') as f:
+    f.write(output_html)
+
+# Open the file in a web browser
+webbrowser.open("file://"+html_file_path+"/output.html")
 
